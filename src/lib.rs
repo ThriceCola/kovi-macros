5fda9ad19e37307854f46f81cd9a268352135295
Threkork <threkork@hotmail.com>
2024-08-22 00:19:04 +0800

    modified:   Cargo.toml 	modified:   src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -11,13 +11,34 @@ use syn::{parse_macro_input, ItemFn};
 pub fn plugin(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as ItemFn);
 
-    let expanded = quote! {
-        #input
+    let is_async = input.sig.asyncness.is_some();
+    let fn_name = &input.sig.ident;
 
-        pub fn __kovi__get_crate_name() -> (&'static str, &'static str) {
-            let name = env!("CARGO_PKG_NAME");
-            let version = env!("CARGO_PKG_VERSION");
-            (name, version)
+    let expanded = if is_async {
+        quote! {
+            #input
+
+            pub fn __kovi__get_plugin_info() -> (&'static str, &'static str) {
+                let name = env!("CARGO_PKG_NAME");
+                let version = env!("CARGO_PKG_VERSION");
+                (name, version)
+            }
+
+            pub fn __kovi__run_async_plugin(p: PluginBuilder) -> std::pin::Pin<Box<dyn std::future::Future<Output = ()> + Send>> {
+                Box::pin(async move {
+                    #fn_name(p).await;
+                })
+            }
+        }
+    } else {
+        quote! {
+            #input
+
+            pub fn __kovi__get_plugin_info() -> (&'static str, &'static str) {
+                let name = env!("CARGO_PKG_NAME");
+                let version = env!("CARGO_PKG_VERSION");
+                (name, version)
+            }
         }
     };
 
