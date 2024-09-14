b4081d2694cc7c414bfa53c2231a9cd632449e86
Threkork <threkork@hotmail.com>
2024-09-14 20:07:43 +0800

    modified:   Cargo.toml 	modified:   src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -10,35 +10,21 @@ use syn::{parse_macro_input, ItemFn};
 #[proc_macro_attribute]
 pub fn plugin(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as ItemFn);
-
-    let is_async = input.sig.asyncness.is_some();
     let fn_name = &input.sig.ident;
 
-    let expanded = if is_async {
-        quote! {
-            #input
+    let expanded = quote! {
+        #input
 
-            pub fn __kovi__get_plugin_info() -> (&'static str, &'static str) {
-                let name = env!("CARGO_PKG_NAME");
-                let version = env!("CARGO_PKG_VERSION");
-                (name, version)
-            }
-
-            pub fn __kovi__run_async_plugin(p: PluginBuilder) -> std::pin::Pin<Box<dyn std::future::Future<Output = ()> + Send>> {
-                Box::pin(async move {
-                    #fn_name(p).await;
-                })
-            }
+        pub fn __kovi__get_plugin_info() -> (&'static str, &'static str) {
+            let name = env!("CARGO_PKG_NAME");
+            let version = env!("CARGO_PKG_VERSION");
+            (name, version)
         }
-    } else {
-        quote! {
-            #input
 
-            pub fn __kovi__get_plugin_info() -> (&'static str, &'static str) {
-                let name = env!("CARGO_PKG_NAME");
-                let version = env!("CARGO_PKG_VERSION");
-                (name, version)
-            }
+        pub fn __kovi__run_async_plugin(p: PluginBuilder) -> std::pin::Pin<Box<dyn std::future::Future<Output = ()> + Send>> {
+            Box::pin(async move {
+                #fn_name(p).await;
+            })
         }
     };
 
